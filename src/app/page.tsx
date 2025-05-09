3810e0fb438936189ad9b88a22dc0e23100bb168
Riquelmi Santos <riquelme.dos.santos.kw@gmail.com>
2025-05-08 23:05:44 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./_components/ui/button";
+
 // Sever
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <p className="text-lg">This is a simple Next.js app with Tailwind CSS.</p>
+      <Button>Teste</Button>
     </main>
   )
 }
