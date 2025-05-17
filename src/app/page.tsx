47f70ed09863e8cb6381911b2b37a3fb522d0ca5
Riquelmi Santos <riquelme.dos.santos.kw@gmail.com>
2025-05-17 13:03:47 -0300

    feat: add header and Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 // Sever
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-lg">This is a simple Next.js app with Tailwind CSS.</p>
-      <Button>Teste</Button>
-    </main>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Riquelmi</h2>
+        <p>Sabado, 17 de Maio.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/Banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
