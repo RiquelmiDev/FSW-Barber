b4deeb64f5756b677e0a714f0c114aacb7726082
Riquelmi Santos <riquelme.dos.santos.kw@gmail.com>
2025-05-17 14:41:48 -0300

    feat: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 // Sever
 export default function Home() {
@@ -10,9 +13,11 @@ export default function Home() {
     <div>
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Riquelmi</h2>
         <p>Sabado, 17 de Maio.</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -20,6 +25,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* Banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -28,6 +34,31 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* Agendamento */}
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/2f9278ba-3975-4026-af46-64af78864494-16u.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia Do Alfonso</p>
+              </div>
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Maio</p>
+              <p className="text-2xl">17</p>
+              <p className="text-sm">08:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
