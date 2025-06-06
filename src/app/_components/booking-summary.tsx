5e119bd0567f9c4d774b41659062e40be7f4f87b
Riquelmi Santos <riquelme.dos.santos.kw@gmail.com>
2025-06-06 00:32:36 -0300

    chore: add prisma command to prepare script

--- src/app/_components/booking-summary.tsx
+++ src/app/_components/booking-summary.tsx
@@ -1,48 +1,5 @@
-/* import { format } from "date-fns"
-import { Card, CardContent } from "./ui/card" */
-
-//TODO:
-
-/* interface BookingSummaryProps {
-
-}
-
 const BookingSummary = () => {
-  return (
-    <Card>
-      <CardContent className="space-y-3 p-3">
-        <div className="flex items-center justify-between">
-          <h2 className="font-bold">{service.name}</h2>
-          <p className="text-sm font-bold">
-            {Intl.NumberFormat("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }).format(Number(service.price))}
-          </p>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-gray-400">Data:</h2>
-          <p className="text-sm">
-            {format(selectedDay, "d 'de' MMMM", {
-              locale: ptBR,
-            })}
-          </p>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-gray-400">Horario:</h2>
-          <p className="text-sm">{selectedTime}</p>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-gray-400">Barbearia:</h2>
-          <p className="text-sm">{barbershop.name}</p>
-        </div>
-      </CardContent>
-    </Card>
-  )
+  return <h1></h1>
 }
 
 export default BookingSummary
- */
